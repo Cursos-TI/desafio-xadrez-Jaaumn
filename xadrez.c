f1d66e42fe3c428d8eb73c82d78fb0a88c29cb5b
Jaaumn <rodriguesjoaopedro6@gmail.com>
2025-04-03 23:58:40 +0000

    Nivel aventureiro - movimentaçao do cavalo

--- xadrez.c
+++ xadrez.c
@@ -2,6 +2,7 @@
 
 int main (){
     int torre = 1, bispo = 1, rainha = 1;
+    int movimentocavalo = 1;
 
     while (torre <=5) //enquanto a condição for verdadeira, roda o loop
     {
@@ -18,8 +19,20 @@ int main (){
     for(rainha; rainha <=8; rainha++)
     {
         printf("# Peça - Rainha #\nMovimento - Esquerda\n");
-        bispo++;
-    } while (bispo <=5);
+    } 
+    
+    //movimentação do cavalo
+    while (movimentocavalo--) //decreção para condiçaõ virar falsa
+    {
+        printf("\n# Peça - Cavalo #\n");
+        for (int i = 0; i < 2; i++)
+        {
+            printf("Movimento - Baixo\n");
+        }
+        printf("Movimento - Esquerda\n");
+    }
+    
+    
     
     
     
